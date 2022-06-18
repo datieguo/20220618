aaa5cc9c6cefddb512494f0c72016d4ea92a65c0
datieguo <36290883+datieguo@users.noreply.github.com>
2022-06-18 11:26:14 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "3c6f9e90-91bb-403d-8582-168ba7270bf9",
                         "alterId": 0
                     }
                 ],
